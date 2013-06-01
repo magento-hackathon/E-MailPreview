56e2f40f915cc9850ab0e617479f0baff5a7f6d5
Tobias Hille <tobihille@googlemail.com>
2013-06-01 21:35:38 +0200

    Added rewrite for Controller Action Mage/Adminhtml/controllers/System/Email/TemplateController->editAction; prepared Source - Model for getting options from config.xml

---
+++ app/code/community/Hackathon/EmailPreview/controllers/Adminhtml/System/Email/TemplateController.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('Mage/Adminhtml/controllers/System/Email/TemplateController.php');
+
+class Hackathon_EmailPreview_Adminhtml_System_Email_TemplateController
+    extends Mage_Adminhtml_System_Email_TemplateController
+{
+
+    /**
+     * Edit transactional email action
+     * override fcuntion to _NOT_ load the Block adminhtml/system_email_template_edit before rendering
+     */
+    public function editAction()
+    {
+        echo "myControllerAdminhtml";
+
+        $this->loadLayout();
+        $template = $this->_initTemplate('id');
+        $this->_setActiveMenu('system/email_template');
+        $this->_addBreadcrumb(Mage::helper('adminhtml')->__('Transactional Emails'), Mage::helper('adminhtml')->__('Transactional Emails'), $this->getUrl('*/*'));
+
+        if ($this->getRequest()->getParam('id')) {
+            $this->_addBreadcrumb(Mage::helper('adminhtml')->__('Edit Template'), Mage::helper('adminhtml')->__('Edit System Template'));
+        } else {
+            $this->_addBreadcrumb(Mage::helper('adminhtml')->__('New Template'), Mage::helper('adminhtml')->__('New System Template'));
+        }
+
+        $this->_title($template->getId() ? $template->getTemplateCode() : $this->__('New Template'));
+
+        /*
+        $this->_addContent($this->getLayout()->createBlock('adminhtml/system_email_template_edit', 'template_edit')
+            ->setEditMode((bool)$this->getRequest()->getParam('id')));
+        */
+
+        $this->renderLayout();
+    }
+
+}
