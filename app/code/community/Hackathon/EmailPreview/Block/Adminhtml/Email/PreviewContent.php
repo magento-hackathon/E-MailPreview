9a174615687fcf9e6a40cac4ad5311f308c3046c
Mathis Klooß <mathis@mage-profis.de>
2015-12-01 14:37:09 +0100

    set correct env to get the correct templates and translations

--- app/code/community/Hackathon/EmailPreview/Block/Adminhtml/Email/PreviewContent.php
+++ app/code/community/Hackathon/EmailPreview/Block/Adminhtml/Email/PreviewContent.php
@@ -8,28 +8,35 @@ class Hackathon_EmailPreview_Block_Adminhtml_Email_PreviewContent extends Mage_A
      */
     protected function _toHtml()
     {
-        $previewModel = Mage::getModel('hackathon_emailpreview/emailPreview');
-        
         $storeId = $this->getRequest()->getParam('storeId');
+        $appEmulation = Mage::getSingleton('core/app_emulation');
+        $initialEnvironmentInfo = $appEmulation->startEnvironmentEmulation($storeId);
+        Mage::app()->getTranslator()->init('frontend', true);
+
+        $previewModel = Mage::getModel('hackathon_emailpreview/emailPreview');
+
         $templateId = $this->getRequest()->getParam('templateId');
         $templateType = $this->getRequest()->getParam('templateType');
 
         $templateParams = new Varien_Object();
         $templateParams->setRequestParams($this->getRequest()->getParams());
         $templateParams->setStoreId($storeId);
-        
+
         $eventData = array(
             'templateParams' => $templateParams,
             'templateType' => $templateType
         );
-        
+
         Mage::dispatchEvent('hackathon_emailpreview_render_email_before', $eventData);
-        
+
         $storeId = $templateParams->getStoreId();
         $templateParams->setStore(Mage::app()->getStore($storeId));
-        
+
         $html = $previewModel->renderEmail($templateId, $templateParams->getData());
-        
+
+        Mage::app()->getTranslator()->init('frontend', false);
+        $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
+
         return $html;
     }
 }
